deed55b1938bd223bb02e72721c5e41852f5155a
huwgeddes <geddes.huw@gmail.com>
2018-10-21 22:43:30 +1100

    add fan and backup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,121 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div>
+    <button @click="show = !show">Click Here</button>
+      <div class="menu-container" style="font-family: sans-serif;width: 0px;height: 0px;top: 0px;left: 0px;">
+        <div class="menu-list" style="top: 200px;left: 300px;width: 100px;height: 100px;">
+
+          <transition name="spin">
+            <div class="wing wing-1" v-if="show">
+              <svg class="wing-svg" height="100%" width="100%" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                <path style="pointer-events: auto; touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" fill="#ea2a29" d="M38,11 L70,-21 A100,100 0 0, 1 70,120 L38,89  A55,55 1 0, 0 38,11 z"></path>
+                <text class="wing-text" dominant-baseline="central" x="50%" y="50%" text-anchor="middle" transform="rotate(90, 60, 60)" >
+                  Add Info
+                </text>
+              </svg>
+            </div>
+          </transition>
+
+            <transition name="spin">
+              <div class="wing wing-2" v-if="show" @click="show = !show">
+                <svg class="wing-svg" height="100%" width="100%" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                  <path style="pointer-events: auto; touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" fill="#f16729" d="M38,11 L70,-21 A100,100 0 0, 1 70,120 L38,89  A55,55 1 0, 0 38,11 z"></path>
+                  <text class="wing-text" dominant-baseline="central" x="50%" y="50%" text-anchor="middle" transform="rotate(270, 60, 40)" >
+                    Cancel
+                  </text>
+                </svg>
+              </div>
+            </transition>
+
+            <transition name="spin">
+              <div class="wing wing-3" v-if="show">
+                <svg class="wing-svg" height="100%" width="100%" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                  <path style="pointer-events: auto; touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" fill="#4d7190" d="M38,11 L70,-21 A100,100 0 0, 1 70,120 L38,89  A55,55 1 0, 0 38,11 z"></path>
+                  <text class="wing-text" dominant-baseline="central" x="50%" y="50%" text-anchor="middle" transform="rotate(90, 60, 60)" >
+                    Go to user
+                  </text>
+                </svg>
+              </div>
+            </transition>
+
+            <transition name="spin">
+              <div class="wing wing-4" v-if="show">
+                <svg class="wing-svg" height="100%" width="100%" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                  <path style="pointer-events: auto; touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" fill="#ffcf14" d="M38,11 L70,-21 A100,100 0 0, 1 70,120 L38,89  A55,55 1 0, 0 38,11 z"></path>
+                  <text class="wing-text" dominant-baseline="central" x="50%" y="50%" text-anchor="middle" transform="rotate(90, 60, 60)" >
+                    Go to user
+                  </text>
+                </svg>
+              </div>
+            </transition>
+
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      show: false
+    }
+  },
+  computed: {
+    wingOne () {
+      return this.show ? 'transform: rotate(30deg) scale(1);' : ''
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .menu-container {
+    position: fixed;
+    z-index: 99999;
+  }
+  .menu-list {
+    position: absolute;
+    pointer-events: none;
+  }
+  .wing {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    transform-origin: 0 50%;
+    cursor: pointer;
+  }
+  .wing-text {
+    font-size: 16px;
+    fill: #fff
+  }
+  .wing path:hover {
+    fill: #000
+  }
+  .wing-svg {
+    overflow: visible;
+  }
+
+  .wing-1 {
+    transform: rotate(0deg) scale(1);
+  }
+  .wing-2 {
+    transform: rotate(90deg) scale(1);
+  }
+  .wing-3 {
+    transform: rotate(180deg) scale(1);
+  }
+  .wing-4 {
+    transform: rotate(270deg) scale(1);
+  }
+
+  .spin-enter-active {
+    transition: all 0.5s ease
+  }
+  .spin-enter {
+    transform: rotate(0) scale(0);
+  }
+
+
 </style>
---
+++ src/components/HelloWorld0.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <button @click="show = !show">Click Here</button>
+      <div class="menu-container" style="font-family: sans-serif; width: 60px; height: 60px; top: 200px; left: 30px; transition: none 0s ease 0s;">
+        <div class="menu-list" style="top: -70px; left: 300px; width: 200px; height: 200px;">
+
+          <transition name="spin-one">
+            <div class="wing wing-1" v-if="show">
+              <svg class="wing-svg" height="100%" width="100%" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                <path style="pointer-events: auto; touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" fill="#ea2a29" d="M53,85 L193,48 A200,200 0 0, 1 193,151 L53,115  A55,55 1 0, 0 53,85 z"></path>
+              </svg>
+            </div>
+          </transition>
+
+            <transition name="spin-two">
+              <div class="wing wing-2" v-if="show">
+                <svg class="wing-svg" height="100%" width="100%" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                  <path style="pointer-events: auto; touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" fill="#f16729" d="M53,85 L193,48 A200,200 0 0, 1 193,151 L53,115  A55,55 1 0, 0 53,85 z"></path>
+                </svg>
+              </div>
+            </transition>
+
+            <transition name="spin-three">
+              <div class="wing wing-3" v-if="show">
+                <svg class="wing-svg" height="100%" width="100%" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                  <path style="pointer-events: auto; touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" fill="#4d7190" d="M53,85 L193,48 A200,200 0 0, 1 193,151 L53,115  A55,55 1 0, 0 53,85 z"></path>
+                </svg>
+              </div>
+            </transition>
+
+            <transition name="spin-four">
+              <div class="wing wing-4" v-if="show">
+                <svg class="wing-svg" height="100%" width="100%" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                  <path style="pointer-events: auto; touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" fill="#ffcf14" d="M53,85 L193,48 A200,200 0 0, 1 193,151 L53,115  A55,55 1 0, 0 53,85 z"></path>
+                </svg>
+              </div>
+            </transition>
+
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      show: false
+    }
+  },
+  computed: {
+    wingOne () {
+      return this.show ? 'transform: rotate(30deg) scale(1);' : ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .menu-container {
+    position: fixed;
+    z-index: 99999;
+  }
+  .menu-list {
+    position: absolute;
+    pointer-events: none;
+  }
+  .wing {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    transform-origin: 0 50%;
+  }
+
+  .wing-1 {
+    transform: rotate(0deg) scale(1);
+  }
+  .wing-1:hover {
+    transition: all 0.5s ease;
+    transform: rotate(0deg) scale(1.2);
+  }
+
+  .wing-2 {
+    transform: rotate(90deg) scale(1);
+  }
+  .wing-2:hover {
+    transition: all 0.5s ease;
+    transform: rotate(90deg) scale(1.2);
+  }
+
+  .wing-3 {
+    transform: rotate(180deg) scale(1);
+  }
+  .wing-3:hover {
+    transition: all 0.5s ease;
+    transform: rotate(180deg) scale(1.2);
+  }
+
+  .wing-4 {
+    transform: rotate(270deg) scale(1);
+  }
+  .wing-4:hover {
+    transition: all 0.5s ease;
+    transform: rotate(270deg) scale(1.2);
+  }
+
+
+
+  .spin-one-enter-active, .spin-two-enter-active, .spin-three-enter-active, .spin-four-enter-active {
+    transition: all 0.5s ease
+  }
+  .spin-one-enter, .spin-two-enter, .spin-three-enter, .spin-four-enter {
+    transform: rotate(0) scale(1);
+  }
+  .spin-one-enter-to {
+    transform: rotate(0deg) scale(1);
+  }
+  .spin-two-enter-to {
+    transform: rotate(90deg) scale(1);
+  }
+  .spin-three-enter-to {
+    transform: rotate(180deg) scale(1);
+  }
+  .spin-four-enter-to {
+    transform: rotate(270deg) scale(1);
+  }
+
+</style>
